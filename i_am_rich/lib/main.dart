2dfd1e24c6cbfc897cc48d6c2d372937ba0db39c
Ausbel <mr.rochahernandez@gmail.com>
2023-05-31 15:15:48 -0600

    yolo

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,25 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MyApp(),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.deepOrange,
-            fontSize: 30,
-            fontWeight: FontWeight.w100,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          title: const Text('Hello World'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://picsum.photos/250?image=10'),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
